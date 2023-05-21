cf25637be7364e8a9353fcafe0056f798baedebb
CanonWiseman <wisemancanon@gmail.com>
2023-05-21 14:49:12 -0500

    added files for 39.3 first project

---
+++ 39.3/src/index.js
@@ -0,0 +1,15 @@
+
+import fruits from './food';
+import {choice, remove} from './helpers';
+
+
+const fruit = choice(fruits);
+console.log(`I'd like one ${fruit}, please`);
+console.log(`Here you go: ${fruit}`);
+console.log("Delicious! May I have another one?");
+
+let remaining = remove(fruit, fruits);
+
+console.log(`I'm sorry, we're all out. We have ${remaining.length} left.`);
+
+
