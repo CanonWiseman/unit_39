363ff30d1c2a8964547147358ea93deea35fc60d
CanonWiseman <wisemancanon@gmail.com>
2023-06-06 12:46:37 -0500

    fixed useAxios bugs

--- react-cards/src/PlayingCardList.js
+++ react-cards/src/PlayingCardList.js
@@ -1,26 +1,31 @@
-import React, { useState } from "react";
-import {v4 as uuid} from "uuid";
-import axios from "axios";
+import React from "react";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
+import { formatCard } from "./helpers";
 import useAxios from "./hooks/useAxios";
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
+
 function CardTable() {
   // const [cards, setCards] = useState([]);
   // const addCard = async () => {
   //   const response = await axios.get(
   //     "https://deckofcardsapi.com/api/deck/new/draw/"
   //   );
+  //   console.log(cards);
+  //   console.log(response);
   //   setCards(cards => [...cards, { ...response.data, id: uuid() }]);
   // };
-  const [cards, addCard] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/");
+  const [cards, addCard] = useAxios(
+    "cards",
+    "https://deckofcardsapi.com/api/deck/new/draw/"
+  );
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
-        <button onClick={addCard}>Add a playing card!</button>
+        <button onClick={() => addCard(formatCard)}>Add a playing card!</button>
       </div>
       <div className="PlayingCardList-card-area">
         {cards.map(cardData => (
--- react-cards/src/hooks/useAxios.js
+++ react-cards/src/hooks/useAxios.js
@@ -1,17 +1,33 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import {v4 as uuid} from "uuid";
 
-async function useAxios(url, initialState = []){
-    const [data, setData] = useState(initialState);
+function useAxios(keyInLS, baseUrl) {
+  const [responses, setResponses] = useLocalStorage(keyInLS);
 
-    const addData = async () => {
-        const response = await axios.get(
-          url
-        );
-        setData(data => [...data, { ...response.data, id: uuid() }]);
-    }    
-    return [data, addData]
+  const addResponseData = async (formatter = data => data, restOfUrl = "") => {
+    const response = await axios.get(`${baseUrl}${restOfUrl}`);
+    setResponses(data => [...data, formatter(response.data)]);
+  };
+
+  const clearResponses = () => setResponses([]);
+
+  return [responses, addResponseData, clearResponses];
 }
 
-export default useAxios;
+function useLocalStorage(key, initialValue = []) {
+  if (localStorage.getItem(key)) {
+    initialValue = JSON.parse(localStorage.getItem(key));
+  }
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+}
+
+export default useLocalStorage;
+
+export { useAxios, useLocalStorage };
